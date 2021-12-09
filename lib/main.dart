c5b054483a6bec8d5dfa56ac5f7a597859678347
VasilyeuUV <vasilyeu.uv@mail.ru>
2021-12-10 01:39:39 +0300

    FirstFA: Create an infinite scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -12,32 +12,63 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      //
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: RandowWords(),
-        ),
-      ),
+    return const MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
 
-class RandowWords extends StatefulWidget {
-  const RandowWords({Key? key}) : super(key: key);
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
 
   @override
-  _RandowWordsState createState() => _RandowWordsState();
+  _RandomWordsState createState() => _RandomWordsState();
 }
 
-class _RandowWordsState extends State<RandowWords> {
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // функция обратного вызова, создаёт парное слово и помещает его в cnhjre ListTile
+        itemBuilder: (context, i) {
+          // для нечётных слов функция добавляет Divider виджет, для визуального разделения записей высотой в 1px
+          if (i.isOdd) {
+            return const Divider(
+              thickness: 2.0,
+            );
+          }
+
+          // возвращает целочисленный результат 0, 1, 1, 2, 2 для вычисления фактического количества пар слов в Списке за вычетом разделителей
+          final index = i ~/ 2;
+
+          // если достигнут предел доступных пар слов, создаётся ещё 10 и добавляются в список
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
